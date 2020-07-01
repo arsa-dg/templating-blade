a99e0f8a1fdceddf70240ad5dca68ec6b3073dcf
arsa dg <arsaadg891@gmail.com>
2020-07-01 22:32:22 +0700

    git commit resources/views/templating-blade/data-tables.blade.php

--- resources/views/templating-blade/data-tables.blade.php
+++ resources/views/templating-blade/data-tables.blade.php
@@ -442,11 +442,11 @@
 @endsection
 
 @push("scripts")
-<script src="{{asset('plugins/datatables/jquery.dataTables.js')}}"></script>
-<script src="{{asset('plugins/datatables-bs4/js/dataTables.bootstrap4.js')}}"></script>
-<!-- <script>
+<script src="{{asset('/adminlte/plugins/datatables/jquery.dataTables.js')}}"></script>
+<script src="{{asset('/adminlte/plugins/datatables-bs4/js/dataTables.bootstrap4.js')}}"></script>
+<script>
   $(function () {
     $("#example1").DataTable();
   });
-</script> -->
+</script>
 @endpush
